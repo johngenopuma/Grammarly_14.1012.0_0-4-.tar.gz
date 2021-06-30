64b2f1274f52fc45690b1225cbf990cadf2c4c39
EscapeSequins <75280565+EscapeSequins@users.noreply.github.com>
2021-06-29 21:12:27 -0600

    Create gdocsExpandedViewUnavailable.common.chunk.js

---
+++ gdocsExpandedViewUnavailable.common.chunk.js
@@ -0,0 +1,53 @@
+(self.webpackChunk = self.webpackChunk || []).push([
+    [249], {
+        42268: (e, a, t) => {
+            t.r(a), t.d(a, {
+                GdocsExpandedViewUnavailable: () => c
+            });
+            var l = t(61658),
+                n = t(5872),
+                s = t(88326),
+                r = t(27378),
+                o = t(43256);
+            const c = e => r.createElement("div", {
+                className: (0, n.cs)(l.gButtonPopup, l.gdocsBeta, l.unavailablePopup)
+            }, r.createElement(s.M, {
+                clickHandler: () => e.close()
+            }, r.createElement("button", {
+                className: l.close
+            })), r.createElement("div", {
+                className: (0, n.cs)(l.header, l.headerUnavailable)
+            }, r.createElement("div", {
+                className: l.cardsIcon
+            }), "Welcome to our beta!"), r.createElement("div", {
+                className: o.dialogMessage
+            }, "Grammarly’s suggestions", r.createElement("br", null), "appear as red underlines in", r.createElement("br", null), "your text. Hover and click", r.createElement("br", null), "to correct."), r.createElement("div", {
+                className: (0, n.cs)(l.bubbleArrow)
+            }, "▲"))
+        },
+        43256: e => {
+            e.exports = {
+                container: "_14pwk",
+                splashOptInContainer: "_2aNuW",
+                title: "_3hcsr",
+                subtitle: "lEJ_N",
+                btnContainer: "_3GzCO",
+                content: "_1G-sh",
+                optInIcon: "_1y8Qy",
+                header: "_21IRQ",
+                optInVideo: "_1IzMn",
+                bold: "_1-W2s",
+                beta: "_1xiL4",
+                button: "_1OjLI",
+                splash: "_3YWO4",
+                large: "rQ_ZI",
+                optIn: "_200N1",
+                offlineDialogHeader: "_3dhsp",
+                largeDocumentDialogHeader: "_2PC_i",
+                sharedDocumentDialogHeader: "tQth7",
+                dialogMessage: "_1tR54",
+                offlineDialogButton: "yNvqg"
+            }
+        }
+    }
+]);
